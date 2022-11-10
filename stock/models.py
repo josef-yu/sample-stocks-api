75f1281b562736ca7f561feceec7db65f354d556
josef-yu <slay3r28@gmail.com>
2022-11-10 19:31:22 +0800

    Add stock and order models + migration

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from model_utils import models as django_model_utils
+from django.contrib.auth.models import User
 # Create your models here.
+class Stock(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=99, decimal_places=2)
+
+class Order(django_model_utils.TimeStampedModel):
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=99, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
