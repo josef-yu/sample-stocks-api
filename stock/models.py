a49c18a9fa1781e97cbf12b0df26d3e3bb677110
josef-yu <slay3r28@gmail.com>
2022-11-10 22:31:39 +0800

    Restructure order and orderitem models

--- stock/models.py
+++ stock/models.py
@@ -7,8 +7,41 @@ class Stock(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=99, decimal_places=2)
 
+class OrderManager(models.Manager):
+    def calculate_total_price(self, items):
+        return sum(
+            item['stock'].price * item['quantity']
+            for item in items
+        )
+
+    def place(self, data):
+        total_price = self.calculate_total_price(data['stocks'])
+
+        order = self.create(
+            total_price=total_price,
+            user=data['user']
+        )
+
+        OrderItem.objects.bulk_create([
+            OrderItem(
+                order=order,
+                stock=item['stock'],
+                quantity=item['quantity'],
+                price=item['stock'].price
+            )
+            for item in data['stocks']
+        ])
+
+        return order
+
 class Order(django_model_utils.TimeStampedModel):
+    total_price = models.DecimalField(max_digits=99, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    objects = OrderManager()
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=99, decimal_places=2)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
